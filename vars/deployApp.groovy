04134a770ccee9f44741cd5f0dd5b40a4e7bddd5
meenakshiraw <77102358+meenakshiraw@users.noreply.github.com>
2025-09-10 14:21:46 +0530

    Create deployApp.groovy

---
+++ vars/deployApp.groovy
@@ -0,0 +1,12 @@
+def call(String composeFile = "docker-compose.yaml") {
+    def IMAGE_TAG = "build-${env.BUILD_NUMBER}"
+    echo "Using image tag: ${IMAGE_TAG}"
+
+    sh """
+        sed -i 's|chattingo-frontend:.*|chattingo-frontend:${IMAGE_TAG}|' ${composeFile}
+        sed -i 's|chattingo-backend:.*|chattingo-backend:${IMAGE_TAG}|' ${composeFile}
+        cat ${composeFile}
+    """
+
+    sh "/usr/local/bin/docker-compose -f ${composeFile} up --build -d"
+}
